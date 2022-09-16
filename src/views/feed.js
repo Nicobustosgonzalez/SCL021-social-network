6f6ae8240f0899f85c32a32e51d85cfff6378676
LissetteQ <lissetteq123@gmail.com>
2022-09-15 21:39:20 -0300

    arreglos en css

--- src/views/feed.js
+++ src/views/feed.js
@@ -8,79 +8,65 @@ import { CreatePost, getPosts } from "../services/databaseservice.js";
 
 export function feedView() {
   const root = document.createElement("div");
-  root.setAttribute ("id", "containerFeed")
+  root.setAttribute("class", "feedview")
 
-  const divNav = document.createElement("div")
-  divNav.setAttribute("class", "divNav")
-  root.appendChild(divNav)
+    //logo de aplicacion
+    const logoFeed = document.createElement("img");
+    logoFeed.src = "./img/logotransparente.png";
+    logoFeed.setAttribute("class", "logoFeed");
+    root.appendChild(logoFeed);
 
-  //IMGLOGOFEED
-  const logoFeed = document.createElement("img")
-  logoFeed.src ="./img/logotransparente.png"
-  logoFeed.setAttribute("id","logoFeed")
-  divNav.appendChild(logoFeed)
-  //INPUT SEARCH 
-  const inputSearchFeed = document.createElement("input")
-  inputSearchFeed.setAttribute("class","inputSearch")
-  inputSearchFeed.setAttribute("placeholder", "Busca amigos")
-  divNav.appendChild(inputSearchFeed)
-  //BTNS NAV 
-  const btnUserNav = document.createElement("button")
-  btnUserNav.setAttribute("class", "btnNav")
-  btnUserNav.textContent ="user"
-  divNav.appendChild(btnUserNav)
- //boton de cerrar sesion
-  const btnLogOutNav = document.createElement("button")
-  btnLogOutNav.setAttribute("class", "btnNav")
-  btnLogOutNav.setAttribute("id", "btnLogOut")
-  btnLogOutNav.setAttribute("type", "submit")
-  btnLogOutNav.textContent ="LogOut"
-  btnLogOutNav.addEventListener("click", ()=>{
-    signOut(auth).then(()=> {
-      //alerta cierre sesion
-      alert("Haz cerrado sesion")
-    })
-  })
-  divNav.appendChild(btnLogOutNav)
+  //IMAGEN USUARIO
+  const profileImg = document.createElement("img");
+  profileImg.setAttribute("class", "profileImg");
+  profileImg.src = "./img/profile-user.png";
+  root.appendChild(profileImg);
+
+  //boton de cerrar sesion
+  const btnLogOut = document.createElement("button");
+  btnLogOut.setAttribute("id", "btnLogOut");
+  btnLogOut.setAttribute("type", "submit");
+  btnLogOut.textContent = "Log Out";
+  btnLogOut.addEventListener("click", () => {
+    signOut(auth).then(() => {
+      //COLOCAR ALERTA PARA CERRAR SESION
+      console.log("Haz cerrado sesión");
+    });
+  });
+  root.appendChild(btnLogOut);
+
+  //BUSCADOR DE AMIGOS
+  const buscador = document.createElement("input");
+  buscador.setAttribute("class", "buscador");
+  buscador.setAttribute("type", "search");
+
+  buscador.setAttribute("placeholder", "Buscar amigos");
+  root.appendChild(buscador);
+
+  //CAJA DE ESTADO Y LOGO
+  const containFeed = document.createElement("div");
+  containFeed.setAttribute("class", "containFeed");
+  root.appendChild(containFeed);
 
   //caja de publicacion
-  const inputFeedState = document.createElement("input")
-  inputFeedState.setAttribute("class","inputFeedState")
-  inputFeedState.setAttribute("id","valueInputFeed")
-  inputFeedState.setAttribute("placeholder","¿Qué estas pensando?")
-  inputFeedState.setAttribute("type","text")
-  root.appendChild (inputFeedState)
-  //btnPublicarFeed 
-  const btnStateFeed = document.createElement("button")
-  btnStateFeed.textContent ="Publicar"
-  btnStateFeed.addEventListener("click", (e)=>{
-    e.preventDefault();
-    const valueInputFeed = document.querySelector("#valueInputFeed").value;
-    console.log(valueInputFeed)
-    //CreatePost("Hola")
-    CreatePost(valueInputFeed)
-    inputClear()
-  });
+  const inputFeedState = document.createElement("input");
+  inputFeedState.setAttribute("class", "inputFeedState");
+  inputFeedState.setAttribute("placeholder", "¿En qué estas pensando?");
+  inputFeedState.setAttribute("type", "text");
+  const btnStateFeed = document.createElement("button");
+  btnStateFeed.setAttribute("class", "btnStateFeed")
+  btnStateFeed.textContent = "Publicar";
+  btnStateFeed.addEventListener("click" , () =>{CreatePost("Hola")});
 
-  //limpiar input despues de publicar 
-  const inputClear = () =>{
-    document.querySelector('#valueInputFeed').value = "";
-  }
-  root.appendChild(btnStateFeed)
-  //{CreatePost("Hola")});
+  containFeed.appendChild(inputFeedState);
 
+  //DIV PARA POSTS
+  const postBox = document.createElement("div");
+  postBox.setAttribute("class", "postBox");
+  root.appendChild(postBox);
 
-//DIV PARA PUBLICACIONES
-  const postsIn = document.createElement("div")
-  postsIn.setAttribute ("class", "divVacio") 
-  root.appendChild(postsIn)
   
   root.appendChild(btnStateFeed);
-  getPosts((post)=> {postsIn.append(JSON.stringify(post))}) 
+getPosts((post)=> {console.log("Estoy dentro del feed", post)}) 
   return root;
 }
-
-
-// let div = document.createElement("div")
-// let p = document.createElement("p")
-// div.append("Some text", p)
